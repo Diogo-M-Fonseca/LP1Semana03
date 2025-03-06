57d9f007c7cf4eceef0bf8ed088ad0c860407f2d
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-06 22:50:15 +0000

    metade feito

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using HeroPerks;
 
 namespace HeroPerk
 {
@@ -6,7 +7,23 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-          
+           string perk = args[0].Trim().ToLower();
+
+           Perks playerPerks = 0;
+
+           int count1 =0;
+           int count2 =0;
+           int count3 =0;
+           int count4 =0;
+
+           foreach (char let in perk)
+           {
+             if (let != 'w' && let != 'a' && let != 's' && let != 'd')
+             {
+                Console.WriteLine("!Unknown perk!");
+             }
+           }
+
         }
     }
 }
