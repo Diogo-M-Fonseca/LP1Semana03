002b8bb58e0a34595da3af95ce4faab368d7d1c2
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-06 23:14:58 +0000

    Codigo completo

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -22,8 +22,43 @@ namespace HeroPerk
              {
                 Console.WriteLine("!Unknown perk!");
              }
+
+             switch (let)
+             {
+                case 'w': count1++; break;
+                case 'a': count2++; break;
+                case 's': count3++; break;
+                case 'd': count4++; break;
+             }
            }
 
+        if (count1 % 2 != 0) playerPerks |= Perks.WarpShift;
+
+        if (count2 % 2 != 0) playerPerks |= Perks.AutoHeal;
+
+        if (count3 % 2 != 0) playerPerks |= Perks.Stealth;
+
+        if (count4 % 2 != 0) playerPerks |= Perks.DoubleJump;
+
+        if (playerPerks != 0)
+        {
+            Console.WriteLine(playerPerks);
+
+            if((playerPerks && Perks.Stealth) != 0 && (playerPerks && Perks.DoubleJump) != 0)
+            {
+                Console.WriteLine("!Silent Jumper!");
+            }
+
+            if ((playerPerks && Perks.AutoHeal)== 0)
+            {
+                Console.WriteLine("!Not gonna make it!");
+            }
+        }
+        else
+        {
+            Console.WriteLine("!No perks at all!");
+            Console.WriteLine("!Not gonna make it!");
+        }
         }
     }
 }
