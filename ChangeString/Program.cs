241e978ee74addd822e34d18d6cdddced36ecfed
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-06 21:07:24 +0000

    Primeira tentativa exercicio 2

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,26 @@ namespace ChangeString
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Insira uma string:");
+            string read1= Console.ReadLine();
+            Console.WriteLine("insira um caráter:");
+            char read2= Console.ReadLine();
+
+            foreach (char i in read1)
+            {
+                if ( i == read2)
+                {
+                    Console.Write("x");
+                }
+                else
+                {
+                    Console.Write(i);
+                }
+
+
+            }
+
+
         }
     }
 }
