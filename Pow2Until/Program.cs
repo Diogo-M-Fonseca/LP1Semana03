563c8d1a0c051c1231551786ce4ee436c38b11f4
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-03-07 16:58:59 +0000

    ultimas pequenas mudanças

--- Pow2Until/Program.cs
+++ Pow2Until/Program.cs
@@ -7,9 +7,9 @@ namespace Pow2Until
         private static void PowersOf2Until5()
         {
             for(int i = 1; i <= (1<<5); i = i << 1)
-            (
+            {
                 Console.WriteLine(i);
-            )
+            }
         }
         private static void Main(string[] args)
         {
